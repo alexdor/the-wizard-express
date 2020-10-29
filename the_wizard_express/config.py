41bb32c35bb0cec4d34a0d06c739f50b383d8b89
Alexandros Dorodoulis <aldor94@gmail.com>
2020-10-29 01:06:02 +0100

    feat: Add initial implementation for trivia corpus

---
+++ the_wizard_express/config.py
@@ -0,0 +1,33 @@
+from os import getenv, path
+
+
+class _config:
+    """
+    A class with all the configuration used accross the projects
+    """
+
+    def __init__(self):
+        self._debug = False
+        self._proc = None
+
+    cache_dir = getenv("CACHE_DIR", path.join(path.realpath("."), ".cache"))
+    """Config.cache_dir The cache directory to store models and other temporary files"""
+
+    @property
+    def debug(self) -> bool:
+        return self._debug
+
+    @debug.setter
+    def debug(self, value: bool) -> None:
+        self._debug = value
+
+    @property
+    def max_proc_to_use(self) -> int:
+        return self._proc
+
+    @max_proc_to_use.setter
+    def max_proc_to_use(self, value: int) -> None:
+        self._proc = value
+
+
+Config = _config()
--- the_wizard_express/corpus/corpus.py
+++ the_wizard_express/corpus/corpus.py
@@ -1,7 +1,42 @@
-from abc import ABC
+import sys
+from abc import ABC, abstractclassmethod
+from typing import List, Union
+
+from datasets import Dataset
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+# TODO: Find a better name for this
+# TODO: Fix the type of this
+class DataType(TypedDict):
+    question: str
+    answer: str
+    context: str
+
+
+RawDataset = Union[List[DataType], Dataset]
 
 
 class Corpus(ABC):
     """
     Abstract class for all the corpus
     """
+
+    @abstractclassmethod
+    def get_corpus(self):
+        pass
+
+
+class TrainTestDataset(ABC):
+    def get_train_data(self) -> RawDataset:
+        return self.dataset["train"]
+
+    def get_test_data(self) -> RawDataset:
+        return self.dataset["test"]
+
+    def get_validation_data(self) -> RawDataset:
+        return self.dataset["validation"]

90087e2e3da34d4b7df267b1fd02b842770ceffa
Alexandros Dorodoulis <aldor94@gmail.com>
2021-01-18 21:07:14 +0100

    refactor: Improve memory usage

--- the_wizard_express/corpus/triviaqa.py
+++ the_wizard_express/corpus/triviaqa.py
@@ -1,17 +1,26 @@
-from os.path import dirname
+from os.path import dirname, lexists
 from pathlib import Path
-from typing import List
+from pickle import load
 
 from datasets import concatenate_datasets, load_dataset
 from datasets.arrow_dataset import Dataset
+from numpy import sort, unique
+from the_wizard_express.utils import (
+    generate_cache_path,
+    pickle_and_save_to_file,
+)
 
 from ..config import Config
 from .corpus import Corpus, TrainTestDataset
 
 
 class TriviaQA(Corpus, TrainTestDataset):
+    __slots__ = ("corpus_path", "percent_of_data_to_keep", "corpus", "dataset")
+
     def __init__(self, percent_of_data_to_keep: float = 0.1) -> None:
-        self.corpus = None
+        Corpus.__init__(self)
+        self.percent_of_data_to_keep = percent_of_data_to_keep
+        self.corpus_path = generate_cache_path("corpus", self)
         dataset = load_dataset(
             "trivia_qa",
             "rc",
@@ -47,31 +56,32 @@ class TriviaQA(Corpus, TrainTestDataset):
             num_proc=Config.max_proc_to_use,
         )
 
-    def get_corpus(self) -> List[str]:
-        if self.corpus is None:
-            dataset = concatenate_datasets(
-                (
-                    self.dataset["train"],
-                    self.dataset["test"],
-                    self.dataset["validation"],
-                )
+    def _build_corpus(self) -> None:
+        if lexists(self.corpus_path):
+            self._corpus = load(open(self.corpus_path, "rb"))
+            return
+
+        dataset = concatenate_datasets(
+            (
+                self.dataset["train"],
+                self.dataset["test"],
+                self.dataset["validation"],
             )
+        )
 
-            dataset = dataset.map(
-                lambda data: {"context": data["context"]},
-                remove_columns=[
-                    "question",
-                    "answer",
-                ],
-                num_proc=Config.max_proc_to_use,
-            ).unique(column="context")
-            dataset.sort()
-            self.corpus = dataset
-
-        return self.corpus
+        dataset = dataset.map(
+            lambda data: {"context": data["context"]},
+            remove_columns=[
+                "question",
+                "answer",
+            ],
+            num_proc=Config.max_proc_to_use,
+        )
+        dataset = tuple(sort(unique(dataset._data.column("context").to_numpy())))
+        pickle_and_save_to_file(dataset, self.corpus_path)
+        self._corpus = dataset
 
     def save_to_disk(self, file_location: str) -> None:
-        corpus = self.get_corpus()
         Path(dirname(file_location)).mkdir(parents=True, exist_ok=True)
         with open(file_location, "w+", encoding="utf-8") as f:
-            f.writelines([f"{line}\n" for line in corpus])
+            f.writelines([f"{line}\n" for line in self.corpus])

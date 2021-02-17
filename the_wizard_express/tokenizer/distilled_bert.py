2f6d5cfc9683cf6e4d9c44da5ae4aa608bea33fa
Alexandros Dorodoulis <aldor94@gmail.com>
2021-02-17 23:20:40 +0100

    feat: Add distilled bert tokenizer

---
+++ the_wizard_express/tokenizer/distilled_bert.py
@@ -0,0 +1,23 @@
+from transformers import AutoTokenizer
+
+from ..config import Config
+from ..corpus import Corpus
+from . import Tokenizer
+
+
+class DistilledBertTokenizer(Tokenizer):
+    __slots__ = ("tokenizer", "_tokenizer_path")
+    friendly_name = "distil_bert_tokenizer"
+
+    def _build(self, corpus: Corpus, path_to_save: str) -> None:
+        model = "distilbert-base-cased-distilled-squad"
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            model,
+            use_fast=True,
+            cache_dir=Config.cache_dir,
+        )
+
+        self.tokenizer.prepare_for_tokenization
+
+    def _load_from_file(self, file: str) -> None:
+        return self._build()
